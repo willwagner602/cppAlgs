3d2d87f9efb669b07c8e8e345c738459c12dc352
willwagner602 <willwagner602@gmail.com>
2017-02-02 08:53:26 -0500

    - initial commit

---
+++ searchAlgs.hpp
@@ -0,0 +1,18 @@
+/*********************************************************************
+** Author: Will Wagner
+** Date: 1/30/17
+** Description:
+*********************************************************************/
+
+#ifndef CPPALGS_ALGORITHMS_H
+#define CPPALGS_ALGORITHMS_H
+
+
+class searchAlgorithms {
+private:
+
+public:
+    int linearSearch(int, int [], int);
+};
+
+#endif //CPPALGS_ALGORITHMS_H
---
+++ sortAlgs.hpp
@@ -0,0 +1,16 @@
+/*********************************************************************
+** Author: Will Wagner
+** Date: 2/1/17
+** Description:
+*********************************************************************/
+
+#ifndef CPPALGS_SORTALGS_H
+#define CPPALGS_SORTALGS_H
+
+
+class sortAlgs {
+    void insertionSort(int &);
+};
+
+
+#endif //CPPALGS_SORTALGS_H
